a8d4298dc7aa864c338a18b26b2ca22f6d407759
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-10-24 19:00:23 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setuptools
+
+# pyproject
+
+from setuptools import setup, find_packages
+
+
+setup(
+  name="dundie",
+  version="0.1.0",
+  description="Reward Point system for Dunder Mifflin",
+  author="Joao Paulo",
+  packages=find_packages(),
+)
