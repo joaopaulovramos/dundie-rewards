4efa0e6b0f274447e2fa1d1086a4d5b96a9e92b8
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-12-18 00:47:03 -0300

    Add CI GA

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
   long_description=read("README.md"),
   long_description_content_type="text/markdown",
   author="Joao Paulo",
+  python_requires=">=3.8",
   packages=find_packages(),
   entry_points={
     "console_scripts": [
