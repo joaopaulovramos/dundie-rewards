abdf8341211cb1d3fae7c438c3c7a82dbd782810
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-12-18 01:14:11 -0300

    refactor: makefile

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,5 +5,5 @@ from dundie.core import load
 @pytest.mark.unit
 def test_load():
     assert len(load(
-      r'D:\Linuxtips\dundie-rewards\tests\assets\people.csv'
+      r'tests/assets/people.csv'
       )) == 2
