a5d985e414e67a01bc9f750115b7d01ffe2907d1
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2023-03-15 18:36:18 -0300

    feat: test data

--- tests/test_load.py
+++ tests/test_load.py
@@ -3,8 +3,21 @@ import pytest
 from dundie.core import load
 from tests.constants import PEOPLE_FILE
 
+EXPECTED_NAMES = ["Jim Halpert", "Dwight Schrute", "Gabe Lewis"]
+EXPECTED_DEPTS = ["Sales", "Directory"]
+EXPECTED_ROLES = ["Salesman", "Sales Manager", "Manager"]
+EMAIL_DOMAIN = "dundlermifflin.com"
+
 
 @pytest.mark.unit
 @pytest.mark.high
 def test_load():
-    assert len(load(PEOPLE_FILE)) == 3
+    result = load(PEOPLE_FILE)
+    assert len(result) == 3
+
+    for line in result:
+        data = line.split(",")
+        assert data[0] in EXPECTED_NAMES
+        assert data[1] in EXPECTED_DEPTS
+        assert data[2] in EXPECTED_ROLES
+        assert EMAIL_DOMAIN in data[3]
