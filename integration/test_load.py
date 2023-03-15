e7c4e31a21022e958e57eff7b57241baf601255a
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2023-03-15 18:00:23 -0300

    feat: new tests

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,12 +1,22 @@
 import pytest
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from tests.constants import PEOPLE_FILE
 
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     out = check_output(
       ["dundie", "load", PEOPLE_FILE]
       ).decode('utf-8').split('\n')
     assert len(out) == 2
+
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
+def test_load_negative_call_load_command(wrong_command):
+    with pytest.raises(CalledProcessError):
+        check_output(
+          ["dundie", wrong_command, PEOPLE_FILE]
+        ).decode('utf-8').split('\n')
