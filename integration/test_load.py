5a3e74df5456f297a10d442d9304e19cfeb49ad8
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2023-03-15 22:12:32 -0300

    add: click and rich

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,26 +1,24 @@
-from subprocess import CalledProcessError, check_output
-
 import pytest
+from click.testing import CliRunner
 
+from dundie.cli import load, main
 from tests.constants import PEOPLE_FILE
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_load_command():
-    out = (
-        check_output(["dundie", "load", PEOPLE_FILE])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(out) == 2
+    out = cmd.invoke(load, PEOPLE_FILE)
+    assert "Dunder Mifflin Associates" in out.output
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command(wrong_command):
-    with pytest.raises(CalledProcessError):
-        check_output(["dundie", wrong_command, PEOPLE_FILE]).decode(
-            "utf-8"
-        ).split("\n")
+    out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
+
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'" in out.output
